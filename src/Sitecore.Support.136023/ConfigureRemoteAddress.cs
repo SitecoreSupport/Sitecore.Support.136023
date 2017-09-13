f19fd66fa6c8c9a02595e72526df7eb54332b195
Pushpraj Ruhal <pur@sitecore.net>
2017-09-13 10:39:48 +0800

    Fix incorrect client IP when Sitecore servers are behind proxy load balancer

---
+++ src/Sitecore.Support.136023/ConfigureRemoteAddress.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Web;
+using Sitecore.Configuration;
+using Sitecore.Pipelines.HttpRequest;
+
+namespace Sitecore.Support
+{
+  public class ConfigureRemoteAddress: HttpRequestProcessor
+  {
+    public override void Process(HttpRequestArgs args)
+    {
+     
+      var configHeader = Settings.GetSetting("JSNLog.RemoteAddressHeader", string.Empty);
+      if (!string.IsNullOrEmpty(configHeader))
+      {
+        var configHeaderValue = HttpContext.Current.Request.ServerVariables[configHeader];
+        if (!string.IsNullOrEmpty(configHeaderValue))
+        {
+          IPAddress ipaddress = null;
+          if (IPAddress.TryParse(configHeaderValue, out ipaddress))
+          {
+            HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"] = ipaddress.ToString();
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.136023/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.136023")]
+[assembly: AssemblyProduct("Sitecore.Support.136023")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Fix incorrect client IP when Sitecore servers are behind proxy load balancer.")]
+
